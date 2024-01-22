4248ab56cd0a2bb22a9b5456a91e2418c06fda39
Diana-PC <carolinabravo946@gmail.com>
2024-01-21 20:16:00 -0500

    Clase 17

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/ECrudEntrenador.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/ECrudEntrenador.kt
@@ -5,9 +5,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.compose.material3.Snackbar
+import com.google.android.material.snackbar.Snackbar
 
 class ECrudEntrenador : AppCompatActivity() {
-    /*override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ecrud_entrenador)
         // Buscar Entrenador
@@ -25,35 +26,61 @@ class ECrudEntrenador : AppCompatActivity() {
                     id.text.toString().toInt()
                 )
             // Setear el texto en componentes visuales
+            if(entrenador.id == 0){
+                mostrarSnackbar("Usu. no encontrado")
+            }
             id.setText(entrenador.id.toString())
             nombre.setText(entrenador.nombre)
             descripcion.setText(entrenador.descripcion)
+            mostrarSnackbar("Usu. encontrado")
+
         }
 
-        val botonActualizarBDD =findViewById<Button>(R.id.btn_actualizar_bdd)
-        botonActualizarBDD.setOnClickListener {
-            val id = findViewById<EditText>(R.id.input_id)
-            val nombre = findViewById<EditText>(R.id.input_nombre)
-            val descripcion = findViewById<EditText>(R.id.input_descripcion)
-            val respuesta = EBaseDeDatos.tablaEntrenador!!.actualizarEntrenadorFormulario(nombre.text.toString(),
-                descripcion.text.toString(),id.text.toString().toInt())
-            if(respuesta) mostrarSnackbar("Usu.Actualizado")
-                    }
-
-        val botonEliminarBDD = findViewById<
-                Button>(R.id.btn_eliminar_bdd)
+        val botonCrearBDD = findViewById<Button>(R.id.btn_crear_bdd)
+        botonCrearBDD
+            .setOnClickListener {
+                val nombre = findViewById<EditText>(R.id.input_nombre)
+                val descripcion = findViewById<EditText>(R.id.input_descripcion)
+                val respuesta = EBaseDeDatos
+                    .tablaEntrenador!!.crearEntrenador(
+                        nombre.text.toString(),
+                        descripcion.text.toString()
+                    )
+                if (respuesta) mostrarSnackbar("Ent. Creado")
+            }
+        val botonActualizarBDD = findViewById<Button>(R.id.btn_actualizar_bdd)
+        botonActualizarBDD
+            .setOnClickListener {
+                val id = findViewById<EditText>(R.id.input_id)
+                val nombre = findViewById<EditText>(R.id.input_nombre)
+                val descripcion = findViewById<EditText>(R.id.input_descripcion)
+                val respuesta = EBaseDeDatos.tablaEntrenador!!.actualizarEntrenadorFormulario(
+                    nombre.text.toString(),
+                    descripcion.text.toString(),
+                    id.text.toString().toInt()
+                )
+                if (respuesta) mostrarSnackbar("Usu. Actualizado")
+            }
+        val botonEliminarBDD = findViewById<Button>(
+            R.id.btn_eliminar_bdd
+        )
         botonEliminarBDD.setOnClickListener {
             val id = findViewById<EditText>(R.id.input_id)
-            val respuesta = EBaseDeDatos.tablaEntrenador!!.eliminarEntrenadorFormulario(id.text.toString().toInt())
-            if(respuesta
-                )mostrarSnackbar("Usu. Eliminado")
+            val respuesta = EBaseDeDatos.tablaEntrenador!!
+                .eliminarEntrenadorFormulario(
+                    id.text.toString().toInt()
+                )
+            if (respuesta) mostrarSnackbar("Usu. Eliminado")
         }
+    }
+    private fun mostrarSnackbar(texto: String) {
 
-        fun mostrarSnackbar(texto: String){
-           Snackbar.make(findViewById(R.id.cl_sqlite),
-               texto,
-               Snackbar.LENGTH_LONG).show
-        }
-    }*/
-
+        Snackbar
+            .make(
+                findViewById(R.id.cl_sqlite), // view
+                texto, // texto
+                Snackbar.LENGTH_LONG // tiempo
+            )
+            .show()
+    }
 }
